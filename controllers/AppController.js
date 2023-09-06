92c9b9aeba15cae84263114f0b6ba7c33112e383
Samuel Nnadozie <75990760+zinando@users.noreply.github.com>
2023-09-06 01:51:32 +0100

    Project completed

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,26 +1,19 @@
+/* eslint-disable import/no-named-as-default */
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
-class AppController {
-  // @desc Get Redis client status
-  // @route GET /status
-  // @access public
+export default class AppController {
   static getStatus(req, res) {
-    res.status(200).send({
+    res.status(200).json({
       redis: redisClient.isAlive(),
       db: dbClient.isAlive(),
     });
   }
 
-  // @desc Get DB client stats
-  // @route GET /stats
-  // @access public
   static getStats(req, res) {
-    res.status(200).send({
-      users: dbClient.nbUsers(),
-      db: dbClient.nbFiles(),
-    });
+    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
+      .then(([usersCount, filesCount]) => {
+        res.status(200).json({ users: usersCount, files: filesCount });
+      });
   }
 }
-
-export default AppController;
